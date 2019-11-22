0891fce8a9253afd045c5501583b52ee844a238a
Maleeha <malehahasan@gmail.com>
2019-11-22 14:33:45 -0500

    API getting the data

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let url = 'https://www.triposo.com/api/20190906/local_highlights.json?latitude=40.783058&longitude=-73.971252';
+let url = 'https://www.triposo.com/api/20190906/city_walk.json?location_id=Paris&total_time=200';
 
 window.onload = function () {
 
@@ -7,7 +7,15 @@ window.onload = function () {
     event.preventDefault()
 
     // axios.get(URL, { headers: { Authorization: AuthStr } })
-    const response = await axios.get(`${url}`, { headers: { "Triposo-Account": 'QGPSD9G3', "Triposo-Token": 'tsrpze8uo75pra7bmbbnrenn18mda8st' } });
-    console.log(response)
+    const result = await axios.get('https://www.triposo.com/api/20190906/city_walk.json?location_id=Paris&total_time=200', { headers: { "X-Triposo-Account": 'QGPSD9G3', "X-Triposo-Token": 'tsrpze8uo75pra7bmbbnrenn18mda8st' } });
+    console.log(result.data)
+
+    for (let i = 0; i < result.data.results[0].way_points.length; i++) {
+
+      console.log(result.data.results[0].way_points[i].poi.name)
+      console.log(result.data.results[0].way_points[i].poi.snippet)
+    }
+
   })
 }
+
