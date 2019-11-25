dca9a57ea71704349878b5019b4a10c8986938fc
Maleeha <malehahasan@gmail.com>
2019-11-25 09:12:32 -0500

    API getting the data-11/25/19

--- main.js
+++ main.js
@@ -1,19 +1,30 @@
-let url = 'https://www.triposo.com/api/20190906/city_walk.json?location_id=Paris&total_time=200';
+let baseUrl = 'https://www.triposo.com/api/20190906/city_walk.json?location_id='
+
+//input Value of City
+// check for first letter is uppercase Cities with more than one word hjave the format New_York_City
+const cityInput = document.querySelector('#city')
+//input value of time
+// check value 0-350 minutes
+const timeInput = document.querySelector('#time')
+const button = document.querySelector('#getCity')
 
 window.onload = function () {
+  const url = `${baseUrl}${cityInput.value}&total_time=${timeInput.value}`;
 
-  const button = document.querySelector('#getCity')
   button.addEventListener('click', async function () {
     event.preventDefault()
 
-    // axios.get(URL, { headers: { Authorization: AuthStr } })
-    const result = await axios.get('https://www.triposo.com/api/20190906/city_walk.json?location_id=Paris&total_time=200', { headers: { "X-Triposo-Account": 'QGPSD9G3', "X-Triposo-Token": 'tsrpze8uo75pra7bmbbnrenn18mda8st' } });
-    console.log(result.data)
+    let url = `${baseUrl}${cityInput.value}&total_time=${timeInput.value}`;
+    //console.log(url)
 
+    //axios.get(URL, { headers: { Authorization: AuthStr } })
+    const result = await axios.get(url, { headers: { "X-Triposo-Account": 'QGPSD9G3', "X-Triposo-Token": 'tsrpze8uo75pra7bmbbnrenn18mda8st' } });
+    //console.log(result.data)
+
+
+    //try to use forEach here
     for (let i = 0; i < result.data.results[0].way_points.length; i++) {
-
-      console.log(result.data.results[0].way_points[i].poi.name)
-      console.log(result.data.results[0].way_points[i].poi.snippet)
+      console.log(`Way Point ${i} ${result.data.results[0].way_points[i].poi.name}:: Description: ${result.data.results[0].way_points[i].poi.snippet}`)
     }
 
   })
