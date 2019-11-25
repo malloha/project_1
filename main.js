a6be596f097c6980c957224f135be711321b2d99
Maleeha <malehahasan@gmail.com>
2019-11-25 11:20:36 -0500

    Data being diplayed on the page

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ const cityInput = document.querySelector('#city')
 // check value 0-350 minutes
 const timeInput = document.querySelector('#time')
 const button = document.querySelector('#getCity')
+const resultContainer = document.querySelector('.results-container')
 
 window.onload = function () {
   const url = `${baseUrl}${cityInput.value}&total_time=${timeInput.value}`;
@@ -19,14 +20,31 @@ window.onload = function () {
 
     //axios.get(URL, { headers: { Authorization: AuthStr } })
     const result = await axios.get(url, { headers: { "X-Triposo-Account": 'QGPSD9G3', "X-Triposo-Token": 'tsrpze8uo75pra7bmbbnrenn18mda8st' } });
-    //console.log(result.data)
-
+    console.log(result.data.results[0].way_points[0])
+    const destinationName = document.createElement('h1')
+    destinationName.innerHTML = `Let's Explore <span> ${cityInput.value} </span> together! `;
+    resultContainer.append(destinationName);
 
     //try to use forEach here
     for (let i = 0; i < result.data.results[0].way_points.length; i++) {
+      const poiName = result.data.results[0].way_points[i].poi.name;
+      const poiDescription = result.data.results[0].way_points[i].poi.snippet;
+      const walkToNextDistance = result.data.results[0].way_points[i].walk_to_next_distance;
+      const walkToNextDuration = result.data.results[0].way_points[i].walk_to_next_duration;
+      const wayPointNumber = i + 1;
+      const destinationImage = result.data.results[0].way_points[i].poi.images[0].sizes.original.url;
+      console.log(destinationImage)
+
+      // create title header h5 element type and assigns class "title-header" to it and adds to the moviecontainer 
+      const destinationStop = document.createElement('div')
+      destinationStop.innerHTML = `Stop ${wayPointNumber}: ${poiName}, Description: ${poiDescription}, Walk to Next Distance ${walkToNextDistance}m, Walk to Next Duration ${walkToNextDuration} mins`
+      resultContainer.append(destinationStop)
+
+      const destinationImg = document.createElement('img')
+      destinationImg.src = `${destinationImage}`;
+      resultContainer.append(destinationImg)
       console.log(`Way Point ${i} ${result.data.results[0].way_points[i].poi.name}:: Description: ${result.data.results[0].way_points[i].poi.snippet}`)
     }
-
   })
 }
 
